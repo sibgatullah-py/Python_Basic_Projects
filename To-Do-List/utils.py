a02d760dc2c8be44fe34da73404158a1b33f488b
Sibgatullah <sibgatullah881@gmail.com>
2025-11-29 18:49:23 +0600

    delete method completed

--- To-Do-List/utils.py
+++ To-Do-List/utils.py
@@ -11,7 +11,9 @@ def load_tasks():
 def save_tasks(tasks):
     with open("tasks.json","w") as file:
         json.dump(tasks, file, indent=2)
-
+'''Use the json module's dump() function to serialize the modified Python object back into JSON format and write it to the file. 
+It's good practice to use indent for readability.
+'''
 
 
         
@@ -56,9 +58,21 @@ def mark_done(task_id):
         
     print("Task not found!")
     
-    
-def delete(tasks_id):
+'''
+To delete an object from a JSON file using Python, 
+the general approach involves reading the JSON data into a Python object (usually a dictionary or a list of dictionaries), 
+modifying that Python object to remove the desired item, and then writing the updated Python object back to the JSON file.
+'''    
+def delete(task_id):
     tasks = load_tasks()
     tasks_list = tasks["tasks"]
     
-    new_list = []
+    new_list = [task for task in tasks_list if task['id'] != task_id]
+    
+    if len(new_list) == len(tasks_list):
+        print("Task not found!")
+        return 
+    
+    tasks['tasks'] = new_list
+    save_tasks(tasks)
+    print(f"Task {task_id} deleted!")
